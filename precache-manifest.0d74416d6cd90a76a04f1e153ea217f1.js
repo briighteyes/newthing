d6e9ccff6c908f88875c5c67e1c68830aed96e2e
Jessica Iliff <jdiliff@Jessicas-MacBook-Pro-2.local>
2019-02-16 12:24:21 -0600

    Updates

---
+++ precache-manifest.0d74416d6cd90a76a04f1e153ea217f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "18a7584d46ca53953aab9bdacab75921",
+    "url": "/static/media/pradyse.18a7584d.otf"
+  },
+  {
+    "revision": "6ee8cf4776654a73615b458a402a2744",
+    "url": "/static/media/kd.6ee8cf47.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "03446ee8953e7d20d182",
+    "url": "/static/js/main.03446ee8.chunk.js"
+  },
+  {
+    "revision": "d0154aeca8b4383960db",
+    "url": "/static/js/2.d0154aec.chunk.js"
+  },
+  {
+    "revision": "03446ee8953e7d20d182",
+    "url": "/static/css/main.8a68730c.chunk.css"
+  },
+  {
+    "revision": "4facdb47a6608cdaa51ce36633f1a7e6",
+    "url": "/index.html"
+  }
+];
